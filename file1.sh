413880e2173de9846f643571f8838b635da49eef
midhun <ec2-user@ip-172-31-41-27.eu-north-1.compute.internal>
2024-06-18 12:20:02 +0000

    added content to branch

---
+++ file1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo enter the values of A
+
+read A
+
+echo enter the value of B
+read B
+echo  addition of two numbers "$((A+B))"
+echo  subtraction of two numbers "$((A-B))"
+echo multiplication of two numbers "$((A*B))"
+echo moduls of two numbers "$((A/B))"
+echo division of two numbers "$((A%B))"
+
